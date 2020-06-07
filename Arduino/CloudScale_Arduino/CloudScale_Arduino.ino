80c19103ae14250805b8c9d44fe69b7975768870
Pavel Zaytsev <pavel@iberisoft.com>
2020-06-07 09:28:11 +0200

    Format tabs

--- Arduino/CloudScale_Arduino/CloudScale_Arduino.ino
+++ Arduino/CloudScale_Arduino/CloudScale_Arduino.ino
@@ -14,75 +14,75 @@ SoftwareSerial gpsSerial(gpsRxPin, gpsTxPin);
 
 void setup()
 {
-  pinMode(ussTriggerPin, OUTPUT);
-  pinMode(ussEchoPin, INPUT);
-  Serial.begin(9600);
-  gpsSerial.begin(9600);
+	pinMode(ussTriggerPin, OUTPUT);
+	pinMode(ussEchoPin, INPUT);
+	Serial.begin(9600);
+	gpsSerial.begin(9600);
 }
 
 void loop()
 {
-  String command = Serial.readStringUntil('\n');
-  if (command == "ID")
-  {
-    Serial.print(deviceId);
-    Serial.print('\n');
-    return;
-  }
-  if (command == "USD")
-  {
-      pollUss();
-      return;
-  }
-  if (command == "GPS")
-  {
-      pollGps();
-      return;
-  }
-  if (command == "R")
-  {
-      pollResistor();
-      return;
-  }
+	String command = Serial.readStringUntil('\n');
+	if (command == "ID")
+	{
+		Serial.print(deviceId);
+		Serial.print('\n');
+		return;
+	}
+	if (command == "USD")
+	{
+		pollUss();
+		return;
+	}
+	if (command == "GPS")
+	{
+		pollGps();
+		return;
+	}
+	if (command == "R")
+	{
+		pollResistor();
+		return;
+	}
 }
 
 void pollUss()
 {
-  digitalWrite(ussTriggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(ussTriggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(ussTriggerPin, LOW);
+	digitalWrite(ussTriggerPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(ussTriggerPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(ussTriggerPin, LOW);
 
-  float duration = pulseIn(ussEchoPin, HIGH);
-  float distance = duration * 0.0343 / 2;
-  Serial.print(distance);
-  Serial.print('\n');
+	float duration = pulseIn(ussEchoPin, HIGH);
+	float distance = duration * 0.0343 / 2;
+	Serial.print(distance);
+	Serial.print('\n');
 }
 
 TinyGPS gps;
 
 void pollGps()
 {
-  float latitude = -1;
-  float longitude = -1;
-  while (gpsSerial.available())
-  {
-    char c = gpsSerial.read();
-    if (gps.encode(c))
-    {
-      gps.f_get_position(&latitude, &longitude);
-    }
-  }
-  Serial.print(latitude);
-  Serial.print(',');
-  Serial.print(longitude);
-  Serial.print('\n');
+	float latitude = -1;
+	float longitude = -1;
+	while (gpsSerial.available())
+	{
+		char c = gpsSerial.read();
+		if (gps.encode(c))
+		{
+			gps.f_get_position(&latitude, &longitude);
+		}
+	}
+	Serial.print(latitude);
+	Serial.print(',');
+	Serial.print(longitude);
+	Serial.print('\n');
 }
 
 void pollResistor()
 {
-  float resistance = (float)analogRead(resistorPin) / maxResistance;
-  Serial.print(resistance);
-  Serial.print('\n');
+	float resistance = (float)analogRead(resistorPin) / maxResistance;
+	Serial.print(resistance);
+	Serial.print('\n');
 }
