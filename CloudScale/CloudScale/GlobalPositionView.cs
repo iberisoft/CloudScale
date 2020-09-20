ecb6ccfb729f20fc175d9bd744e4a14842f02d33
Pavel Zaytsev <pavel@iberisoft.com>
2020-09-20 09:38:17 +0200

    Add GlobalPositionView

---
+++ CloudScale/CloudScale/GlobalPositionView.cs
@@ -0,0 +1,16 @@
+﻿using CloudScale.Shared;
+using Xamarin.Forms;
+
+namespace CloudScale
+{
+    public class GlobalPositionView : ContentView
+    {
+        public static readonly BindableProperty GlobalPositionProperty = BindableProperty.Create(nameof(GlobalPosition), typeof(GlobalPosition), typeof(GlobalPositionView));
+
+        public GlobalPosition GlobalPosition
+        {
+            get => (GlobalPosition)GetValue(GlobalPositionProperty);
+            set => SetValue(GlobalPositionProperty, value);
+        }
+    }
+}
